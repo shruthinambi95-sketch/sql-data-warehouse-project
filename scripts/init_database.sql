726df3f8e79323f0192273ca4f5a747a58a2bebc
shruthinambi95-sketch <shruthinambi95@gmail.com>
2026-01-06 11:52:54 +0530

    Update init_database.sql

--- scripts/init_database.sql
+++ scripts/init_database.sql
@@ -1,5 +1,14 @@
+/* Create database and Schemas
+Script Purpose: 
+  This script creates a new database name "Data_Ware_House" after checking if it already exists. 
+  If the database exists, it is dropped and recreated.
+  Additionally, the script sets up three schemas within the database "bronze", "silver" and "gold".
 
-
+Warning:
+  Runnning this script will drop the entire "Data_Ware_House" database if it exists. 
+  All data in the database will be permanentlu deleted. 
+  Proceed with caution and ensure you have proper backups before running this scrip.
+*/
 
 IF EXISTS (SELECT 1 from sys.databases Where name= 'Data_Ware_House')
   BEGIN
